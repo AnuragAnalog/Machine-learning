64df08ae3e49561c23304b28e242233ab89c7e2f
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-03-21 16:52:01 +0530

    added some code

--- Multiple-Linear-Regression/Gradientdescent/gradientdescent.py
+++ Multiple-Linear-Regression/Gradientdescent/gradientdescent.py
@@ -6,7 +6,7 @@
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 
 class Gradent_descent():
     """
@@ -19,10 +19,7 @@ class Gradent_descent():
         self.__lambda = 0.0
 
     def __str__(self) -> str:
-        if self.__beta >= 0:
-            return 'Regression Line\n'+str(self.__alpha)+' + '+str(self.__beta)+' x'
-        else:
-            return 'Regression Line\n'+str(self.__alpha)+str(self.__beta)+' x'
+        return "Reg coef"+str(self.__coef)
 
     def __cost_function(self, x: np.array, y: np.array) -> float:
         # Calculates the cost of the given regression line.
@@ -31,13 +28,14 @@ class Gradent_descent():
             return
 
         J = 0
-        for (xi, yi) in zip(x, y):
-            J = J + (yi - (self.__beta*xi - self.__alpha))**2
+        tmp_x = np.append(x, [1])
+        tmp_x = tmp_x * self.__coef
+        J = np.sum(tmp_x, axis=1)
         J = J/len(x)
 
         return J
 
-    def fitting(self, x: np.array, y: np.array, alpha: float =None, beta: float =None, learning_rate: float =None, iterate: int =True) -> (float, float):
+    def fitting(self, x: np.array, y: np.array, learning_rate: float =None, iterate: int =True) -> (float, float):
         """
         Fit a linear model
 
@@ -47,54 +45,47 @@ class Gradent_descent():
             A 1-d array which represents features array of the data
         y : ndarray, list
             A 1-d array which represents labels of the data
-        alpha : float, default None
-            Intercept of the model
-        beta : float, default None
-            Slope of the model
         learning_rate : float, default None
             Learning rate for the convergence
-        lambda : float, default 0
-            Regularization rate
         iterate : int, default True
             Number of iterations
 
         Returns
         -------
-        __alpha : float
-            calculated intercept
-        __beta : float
-            calculated slope
+        __coef : ndarray
+            Calculated Coefficients
         """
 
         if len(x) != len(y):
             print("Dimension mismatch of x and y")
             return
 
+        self.data_length =len(x)
+        self.__coef = np.zeros((4, 1)).T
+
+        train_x = np.append(x, np.ones((self.data_length, 1)), axis=1)
+
         if learning_rate is not None:
             self.__learning_rate = learning_rate  # Learning rate
-        self.__beta = beta or 0
-        self.__alpha = alpha or 0
 
         while iterate:
-            __beta_new = self.__beta - self.__learning_rate * ((-2/len(x))*sum((y-(self.__beta*x-self.__alpha))*x))
-            __alpha_new = self.__alpha - self.__learning_rate * ((-2/len(x))*sum(y-self.__beta*x-self.__alpha))
+            print(np.sum((y - np.sum((train_x * self.__coef), axis=1).reshape(self.data_length, -1)) * train_x))
+            __coef_new = self.__coef - self.__learning_rate * ((-2/self.data_length)*np.sum((y - np.sum((train_x * self.__coef), axis=1).reshape(self.data_length, -1))*train_x))
 
             if iterate is True:
                 # Break the loop when the coeffiecients are unchanged upto the first three decimals
                 # When number of iterations are not provided.
-                if np.round(__beta_new, 3) == np.round(self.__beta, 3) and np.round(__alpha_new, 3) == np.round(self.__alpha, 3):
+                if all(np.round(__coef_new, 3) == np.round(self.__coef, 3)):
                     break
             else:
                 iterate = iterate - 1
 
-            self.__beta = np.round(__beta_new, 3)
-            self.__alpha = np.round(__alpha_new, 3)
+            self.__coef = np.round(__coef_new, 3)
 
-        self.__beta = __beta_new
-        self.__alpha = __alpha_new
+        self.__coef = __coef_new
         print("The Cost Function with the obtained parameters is", self.__cost_function(x, y))
 
-        return self.__alpha, self.__beta
+        return self.__coef
 
     def predict(self, x_new) -> float:
         """
@@ -114,12 +105,8 @@ class Gradent_descent():
 
 if __name__ == "__main__":  # This code is for testing purpose
     n = int(input("Enter the np. of data points: "))
+    data = pd.read_csv('../dataset.csv')
+    x = data[['X', 'Y', 'Z']].values
+    y = data[['Length']].values
     reg = Gradent_descent()
-    # x, y = input_data(n)
-    x = np.array([23,26,30,34,43,48])
-    y = np.array([651,762,856,1063,1190,1298])
-    a, b = reg.fitting(x, y, 0.0001, 2000)
-    yp = b*x + a
-    print(len(yp))
-
-    print("The regrssion function is: y = x*{} + {}".format(b, a))
+    print(reg.fitting(x, y, 0.0001, 2000))
