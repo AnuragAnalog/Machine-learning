f7a0e4badb78f6aa8a55fcd17159a74a1a8b86c2
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-03-22 13:42:40 +0530

    added non vectorized implementation

--- Multiple-Linear-Regression/Gradientdescent/gradientdescent.py
+++ Multiple-Linear-Regression/Gradientdescent/gradientdescent.py
@@ -23,17 +23,28 @@ class Gradent_descent():
 
     def __cost_function(self, x: np.array, y: np.array) -> float:
         # Calculates the cost of the given regression line.
+
         if len(x) != len(y):
             print("Dimension mismatch of x and y")
             return
 
         J = 0
-        tmp_x = np.append(x, [1])
+        tmp_x = np.append(x, np.ones((self.data_length, 1)), axis=1)
         tmp_x = tmp_x * self.__coef
         J = np.sum(tmp_x, axis=1)
-        J = J/len(x)
+        J = np.sum(J)/len(x)
 
         return J
+
+    def get_params(self):
+        """
+        Returns
+        -------
+        coef : ndarray
+            returns an 1-d array of coefficients
+        """
+
+        return self.__coef
 
     def fitting(self, x: np.array, y: np.array, learning_rate: float =None, iterate: int =True) -> (float, float):
         """
@@ -60,17 +71,24 @@ class Gradent_descent():
             print("Dimension mismatch of x and y")
             return
 
-        self.data_length =len(x)
-        self.__coef = np.zeros((4, 1)).T
+        self.data_length = len(x)
+        self.features = x.shape[1]
+        self.__coef = np.zeros((self.features+1, 1))
 
+        __coef_new = np.empty_like(self.__coef)
         train_x = np.append(x, np.ones((self.data_length, 1)), axis=1)
 
         if learning_rate is not None:
             self.__learning_rate = learning_rate  # Learning rate
 
         while iterate:
-            print(np.sum((y - np.sum((train_x * self.__coef), axis=1).reshape(self.data_length, -1)) * train_x))
-            __coef_new = self.__coef - self.__learning_rate * ((-2/self.data_length)*np.sum((y - np.sum((train_x * self.__coef), axis=1).reshape(self.data_length, -1))*train_x))
+            print(self.__coef)
+            for j in range(self.features+1):
+                tmp_grad = 0
+                for i in range(self.data_length):
+                    tmp_grad = ((y[i] - np.sum(train_x[i] * self.__coef)) * train_x[i][j])/self.data_length
+                __coef_new[j] = self.__coef[j] - self.__learning_rate * tmp_grad
+            # __coef_new = self.__coef - self.__learning_rate * ((-2/self.data_length)*np.sum((y - np.sum((train_x * self.__coef), axis=1).reshape(self.data_length, -1))*train_x))
 
             if iterate is True:
                 # Break the loop when the coeffiecients are unchanged upto the first three decimals
@@ -101,12 +119,17 @@ class Gradent_descent():
         y_hat : float
             predicted label
         """
-        pass
+
+        tmp_x = np.append(x_new, [1], axis=1)
+        y_hat = tmp_x * self.__coef
+
+        return y_hat
 
 if __name__ == "__main__":  # This code is for testing purpose
     n = int(input("Enter the np. of data points: "))
     data = pd.read_csv('../dataset.csv')
-    x = data[['X', 'Y', 'Z']].values
-    y = data[['Length']].values
+    y = data[['price']].values
+    data.drop(['price'], axis=1, inplace=True)
+    x = data.values
     reg = Gradent_descent()
-    print(reg.fitting(x, y, 0.0001, 2000))
+    print(reg.fitting(x, y, 0.0001, 20))
