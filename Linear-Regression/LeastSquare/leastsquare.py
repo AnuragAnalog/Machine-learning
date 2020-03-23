bf621540de9944c5eb29efe5ee96ede72ba3ebfc
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-03-23 17:29:39 +0530

    added some docstrings

--- Linear-Regression/LeastSquare/leastsquare.py
+++ Linear-Regression/LeastSquare/leastsquare.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
 
-"""
-    ### Simple Linear Regression ###
-    Computes the regression function by taking x and y points, and also
-    plots a graph of the line along with the data-points.
-"""
-
 import numpy as np
-import matplotlib.pyplot as plt
-
-def input_data(n: int) -> (np.array, np.array):
-    x = list()
-    y = list()
-
-    for i in range(n):
-        x.append(float(input("Enter the value of x{}: ".format(i+1))))
-        y.append(float(input("Enter the value of y{}: ".format(i+1))))
-        print("")
-    return np.array(x), np.array(y)
 
 class Regression():
+    """
+        ### Simple Linear Regression ###
+        Computes the regression function by taking x and y points, and also
+        plots a graph of the line along with the data-points.
+    """
+
     def __init__(self) -> None:
         self.__alpha = 0.0
         self.__beta = 0.0
@@ -45,13 +34,105 @@ class Regression():
 
         return J
 
+    def __analysis(self, x: np.array, y: np.array) -> list:
+        # Does some basic analysis and returns a list of some useful parameters. Format of the list is [self.rse, n-p-1, rsq, adjr2, var_hat]
+
+        yp = self.__alpha + self.__beta * x
+
+        p = 1 # No. of regressors
+        self.rss = sum((y - yp)**2)
+        self.rse = np.sqrt(self.rss/(self.data_length - p - 1))
+        self.tss = sum((y - np.mean(y))**2)
+        self.rsq = 1 - (self.rss/self.tss)
+        self.adjr2 = 1 - ((self.rss/(self.data_length - p))/(self.tss/(self.data_length - 1)))
+        self.var_hat = self.rss/(self.data_length - 2)
+
+        return
+
+    def get_rss(self):
+        """
+        Returns
+        -------
+        rss : float
+            Calculated Residual square sum
+        """
+
+        return self.rss
+
+    def get_tss(self):
+        """
+        Returns
+        -------
+        tss : float
+            Calculated Total square sum
+        """
+
+        return self.tss
+
+    def get_rse(self):
+        """
+        Returns
+        -------
+        rse : float
+            Calculated Residual square Error
+        """
+
+        return self.rse
+
+    def get_rsquare(self):
+        """
+        Returns
+        -------
+        rsq : float
+            Calculated R^2
+        """
+
+        return self.rsq
+
+    def get_adjustedr2(self):
+        """
+        Returns
+        -------
+        adjr2 : float
+            Calculated Adjusted R^2
+        """
+
+        return self.adjr2
+
+    def get_estimated_variance(self):
+        """
+        Returns
+        -------
+        var_hat : float
+            Calculated estimate variance
+        """
+
+        return self.var_hat
+
     def fitting(self, x: np.array, y: np.array) -> (float, float):
         """
-            Computes the regression co-efficients for a simple linear regrssion.
+        Computes the regression co-efficients for a simple linear regself.rssion.
+
+        Parameters
+        ----------
+        x : ndarray
+            An array which represents features of the data
+        y : ndarray, list
+            An array which represents labels of the data
+
+        Returns
+        -------
+        __alpha : float
+            Intercept of the linear model
+        __beta : float
+            Slope of the linear model
         """
+
         if len(x) != len(y):
             print("Dimension mismatch of x and y")
             return
+
+        self.data_length = len(x)
 
         self.__beta = (np.mean(x*y)-np.mean(x)*np.mean(y))/(np.mean(x**2)-np.mean(x)**2)
         self.__alpha = np.mean(y) - (self.__beta * np.mean(x))
@@ -59,49 +140,29 @@ class Regression():
 
         return self.__alpha, self.__beta
 
-    def plotting(self, x: np.array, y: np.array, yp: np.array) -> None:
+    def predict(self, x_new):
         """
-            Gives some of the regrssion plots for analysis
+        Predicting the value of a new observation
+
+        Parameters
+        ----------
+        x_new : float
+            A new observation
+
+        Returns
+        -------
+        y_hat : float
+            Predicted value
         """
-        fig = plt.figure()
 
-        fig.add_subplot(2,1,1)
-        plt.plot(x, y, "ro")
-        plt.plot(x, yp)
+        y_hat = self.__alpha + self.__beta * x_new
 
-        fig.add_subplot(2,1,2)
-        plt.scatter(yp, y-yp)       # Residual plot
-
-        plt.show()
-
-    def analysis(self, x: np.array, y: np.array, yp: np.array, n: int) -> list:
-        """
-            Does some basic analysis and returns a list of some useful parameters.
-        Format of the list is [rse, n-p-1, rsq, adjr2, var_hat]
-        """
-        if not all(np.array([len(x), len(y), len(yp)]) != n):
-            print("Dimension mismatch")
-            return
-
-        p = 1 # No. of regressors
-        rss = sum((y - yp)**2)
-        rse = np.sqrt(rss/(n-p-1))
-        tss = sum((y-np.mean(y))**2)
-        rsq = 1 - (rss/tss)
-        adjr2 = 1 - ((rss/(n-p))/(tss/(n-1)))
-        var_hat = rss/(n-2)
-
-        print("Residual sum of squares:",rse,"on",n-p-1,"degrees of freedom.")
-        print("R-squared:",rsq)
-        print("Adjusted R-squared:",adjr2)
+        return y_hat
 
 if __name__ == "__main__":	# Only for testing
-    n = int(input("Enter the np. of data points: "))
     reg = Regression()
-    # x, y = input_data(n)
     x = np.array([23,26,30,34,43,48])
     y = np.array([651,762,856,1063,1190,1298])
     a, b = reg.fitting(x, y)
-    yp = lambda x: b*x + a
 
-    print("The regrssion function is: y = x*{} + {}".format(b, a))
+    print("The regression function is: y = x*{} + {}".format(b, a))
